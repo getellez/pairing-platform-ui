fd069de3c0d2d1b1664109f235d3488f359a09ee
German Tellez <gertellezv@gmail.com>
2022-07-05 22:43:39 -0500

    Add BEM methodology

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Pairing Platform
 This platform is based on the Pair Programming methodology. You add team members and some tasks then this app assign a team member to a task.
+
+# Resources
+Techonologies and methodologies used on this project:
+
+* HTML
+* CSS (Grid, flexbox, BEM)
+* React
--- src/pages/Dashboard/dashboard.jsx
+++ src/pages/Dashboard/dashboard.jsx
@@ -9,89 +9,44 @@ import monkeyOldAvatarUrl from './img/monkey_old_avatar.jpeg';
 export const Dashboard = () => {
   return (
     <section className='dashboard'>
-      <section className='controlPanel'>
-        <div>
-          <button>Pair</button>
+      <section className='control'>
+        <div className='control__action'>
+          <p>Pair</p>
         </div>
-        <div>
-          <button>Save</button>
+        <div className='control__action'>
+          <p>Save</p>
         </div>
-        <div>
-          <button>Cancel</button>
+        <div className='control__action'>
+          <p>Cancel</p>
         </div>
       </section>
-      <p>Team members</p>
-      <section className='membersList'>
-        <div className="membersList-container">
-          <img className='avatar' src={addMemberAvatarUrl} alt="" width={100} />
-          <img className='avatar' src={monkeyAvatarUrl} alt="" width={100} />
-          <img className='avatar' src={monkeyAdidasAvatarUrl} alt="" width={100} />
-          <img className='avatar' src={monkeyOldAvatarUrl} alt="" width={100} />
+      <p className='dasboard__title'>Team members</p>
+      <section className='members'>
+        <div className="members__container">
+          <img className='members__avatar members__avatar-new' src={addMemberAvatarUrl} alt="" width={100} />
+          <img className='members__avatar' src={monkeyAvatarUrl} alt="" width={100} />
+          <img className='members__avatar' src={monkeyAdidasAvatarUrl} alt="" width={100} />
+          <img className='members__avatar' src={monkeyOldAvatarUrl} alt="" width={100} />
         </div>
       </section>
       
-      <section className='boardsList'>
+      <section className='tasks'>
 
-        <div className='boardsList__item'>
-          <div className='boardsList__item-task'>
-            <p className='boardsList__item-title'>View Invoice Summary Document</p>
+        <div className='tasks__column'>
+          <div className='tasks__header-container'>
+            <p className='tasks__header-title'>View Invoice Summary Document</p>
           </div>
-          <div className='boardsList__item__members-container'>
-            <div className='boardsList__item__members-member'>
-              <img className='avatar' src={monkeyAvatarUrl} alt="" width={70}/>
+          <div className='tasks__member'>
+            <div className='tasks__member-container'>
+              <img className='tasks__members-avatar' src={monkeyAvatarUrl} alt="" width={70}/>
             </div>
             <div className='boardsList__item__members-member'>
-              <img className='avatar' src={monkeyOldAvatarUrl} alt="" width={70}/>
+              <img className='tasks__members-avatar' src={monkeyOldAvatarUrl} alt="" width={70}/>
             </div>
           </div>
-          
         </div>
 
-        <div className='boardsList__item'>
-          <div className='boardsList__item-task'>
-            <p className='boardsList__item-title'>Retro authorization table</p>
-          </div>
-          <div className='boardsList__item__members-container'>
-            <div className='boardsList__item__members-member'>
-              <img className='avatar' src={monkeyAvatarUrl} alt="" width={70}/>
-            </div>
-            <div className='boardsList__item__members-member'>
-              <img className='avatar' src={monkeyAdidasAvatarUrl} alt="" width={70}/>
-            </div>
-          </div>
-          
-        </div>
-
-        <div className='boardsList__item'>
-          <div className='boardsList__item-task'>
-            <p className='boardsList__item-title'>Prod Support</p>
-          </div>
-          <div className='boardsList__item__members-container'>
-            <div className='boardsList__item__members-member'>
-              <img className='avatar' src={monkeyAvatarUrl} alt="" width={70}/>
-            </div>
-            <div className='boardsList__item__members-member'>
-              <img className='avatar' src={monkeyAdidasAvatarUrl} alt="" width={70}/>
-            </div>
-          </div>
-          
-        </div>
         
-        <div className='boardsList__item'>
-          <div className='boardsList__item-task'>
-            <p className='boardsList__item-title'>E2E Testing</p>
-          </div>
-          <div className='boardsList__item__members-container'>
-            <div className='boardsList__item__members-member'>
-              <img className='avatar' src={monkeyAvatarUrl} alt="" width={70}/>
-            </div>
-            <div className='boardsList__item__members-member'>
-              <img className='avatar' src={monkeyOldAvatarUrl} alt="" width={70}/>
-            </div>
-          </div>
-          
-          
-        </div>
 
       </section>
 
