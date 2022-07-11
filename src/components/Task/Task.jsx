fcdd51bd9e4dce5b16706d971eed626a3a1decaf
German Tellez <gertellezv@gmail.com>
2022-07-10 23:27:56 -0500

    Add antd input for the task title

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,13 +1,23 @@
 import './Task.css'
 import { DroppableTaskMember } from '../DroppableTaskMember/DroppableTaskMember';
+import { Input } from 'antd'
+import 'antd/lib/input/style/index.css'
+import { useState } from 'react';
 
 export const Task = ({ id, title, members }) => {
+  const [taskTitle, setTaskTitle] = useState('Type new task title...')
+
+  const  handleOnChangeTaskTitle = ({ target }) => {
+    setTaskTitle(target.value)
+  }
+
   return (
     <div
       className='tasks__column'
     >
       <div className='tasks__header-container'>
-        <p className='tasks__header-title'>{title}</p>
+        {/* <p className='tasks__header-title'>{title}</p> */}
+        <Input placeholder='Type a new title...' bordered={false} onChange={handleOnChangeTaskTitle} value={taskTitle} />
       </div>
       <DroppableTaskMember id={id} members={members} />
       
