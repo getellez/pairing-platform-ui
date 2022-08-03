4cb01a930389e9fafad797315e134936e2bae8a6
German Tellez <gertellezv@gmail.com>
2022-08-03 15:32:22 -0500

    Add app name to the dashboard

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,14 @@
 import { NavLink } from 'react-router-dom'
 import { urls } from '../../config/urls';
 import './navbar.css'
+import { APP_NAME } from '../../utils/constants';
 
 export const Navbar = () => {
   return (
     <section className="navbar">
         <h1>
           <NavLink to={urls.homePage}>
-            Pairing Platform
+            <h1 className='App__name'> { APP_NAME } </h1>
           </NavLink>
         </h1>
     </section>
